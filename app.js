918ab458dae1ad78cf6c8d0c3e9b292715bb5f93
shuangliz <358355029@qq.com>
2020-03-13 13:33:18 +0800

    modify app.js

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-const express = require('express')
-const history = require('connect-history-api-fallback')
-const env = process.env.NODE_ENV || 'development'
-
-const path = require('path')
-const app = express()
-
-app.use(history())
-
-if (env !== 'development') {
-  app.use(express.static(path.join(__dirname, 'dist/deploy')))
-}
-
-// 错误处理
-app.use((err, req, res, next) => {
-  res.status(442).send({ error: err.message })
+// app.js
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const chalk = require('chalk')
+const app = express();
+app.use(express.static(path.resolve(__dirname, './dist/deploy')))
+ 
+app.get('*', function(req, res) {
+    const html = fs.readFileSync(path.resolve(__dirname, './dist/deploy/index.html'), 'utf-8')
+    res.send(html)
 })
-
-const server = app.listen(8086, () => {
-  console.log(`Express started in ${app.get('env')} mode on http://127.0.0.1:8080`)
-})
+app.listen(8080, res => {
+  console.log(chalk.yellow('Start Service On 8080'));
+});
